56e5ac8404b754c68a3e211eae439de3fbba2833
Mohamed <mohamedjahha0@gmail.com>
2020-11-20 10:33:40 +0100

    todo app upgraded

--- src/App.js
+++ src/App.js
@@ -10,20 +10,31 @@ export class App extends Component {
     {
       id:1,
       name: 'faire la priere ',
-      completed:false
+      completed:false,
+      date:"2020-11-12",
+      color:""
     },
     {
       id:2,
       name: 'to-do app challenge ',
-      completed:false
+      completed:false,
+      date:"2020-03-18",
+      color:""
+      
     },
     {
       id:3,
       name: 'prepare one to one meeting',
-      completed:false
+      completed:false,
+      date:"1993-04-06",
+      color:""
     } 
   ]
   };
+
+  searchTodo=(searchValue)=>{
+    this.setState({todos:[...this.state.todos.filter((el)=> el.name.includes([searchValue]))]})
+  }
 
   // delete todo
   deleteTodo =(id)=>{
@@ -45,18 +56,40 @@ export class App extends Component {
     const newTodo ={
       id:this.state.todos.length + 1 ,
       name:inputValue,
-      completed:false
+      completed:false,
+      date:"2020-01-01",
+      color:""
     };
     this.setState({todos: [...this.state.todos , newTodo] })
   }
+  saveChanges =(id, editNameValue,editDateValue,editColorValue)=>{
+    
+    this.setState({todos: this.state.todos.filter((el,i)=>{
+      if(el.id===id ){
+          el.name=editNameValue;
+          el.date=editDateValue;
+          el.color=editColorValue
+      }
+        return el
+    })})
+    
+  }
  
+ 
+  
 
   render() {
     return (
       <div>
-        <div className="card bg-light" style={{ width: "700px", margin: "50px auto" }}>
+        <div className="card bg-light" style={{ width: "1000px", margin: "50px auto" }}>
               <Todoform addTodo={this.addTodo}/>
-              <Todolist todos={this.state.todos} checkCompleted={this.checkCompleted} deleteTodo={this.deleteTodo}/>
+              <Todolist 
+              todos={this.state.todos} 
+              checkCompleted={this.checkCompleted} 
+              deleteTodo={this.deleteTodo}
+              saveChanges={this.saveChanges}
+              searchTodo={this.searchTodo}
+              />
         </div>
           </div>
     )
--- src/Components/Todo list component/Todolist.js
+++ src/Components/Todo list component/Todolist.js
@@ -1,35 +1,67 @@
-
-import React, { Component } from 'react'
-import Todoitem from '../Todo item component/Todoitem'
-
+import React, { Component } from 'react';
+import Todoitem from '../Todo item component/Todoitem';
 
 class Todolist extends Component {
+  state = {
+    search: false,
+    searchValue: '',
+  };
 
-     
+  Search = () => {
+    this.setState({ search: !this.state.search });
+  };
 
-    render() {
-       
-        return (
-            <div>
-                <div className="card-body">
-                    <table className="table table-bordered">
-                        <thead className="thead-dark">
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col" style={{width:"500px"}}>Name </th>
-                                <th scope="col" colSpan="3" style={{textAlign:"center"}}>Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.props.todos.map((todo,i)=> <Todoitem key={todo.id} todo={todo} checkCompleted={this.props.checkCompleted} deleteTodo={this.props.deleteTodo}/>)
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const searchForm = (
+      <input
+        type="search"
+        className="form-control w-75"
+        placeholder="Search..."
+        value={this.state.searchValue}
+        onChange={(e) => {
+          alert("Change.....")
+          this.setState({ searchValue: e.target.value });
+          this.props.searchTodo(this.state.searchValue);
+        }}
+      />
+    );
+    return (
+      <div>
+        <div className="card-body">
+          <table className="table table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th
+                  scope="col"
+                  style={{ width: '500px' }}
+                  onDoubleClick={this.Search}
+                >
+                  {this.state.search ? searchForm : 'name'}{' '}
+                </th>
+                <th scope="col" colSpan="3" style={{ textAlign: 'center' }}>
+                  Handle
+                </th>
+                <th scope="col">Date Line</th>
+                <th scope="col">Importance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.todos.map((todo, i) => (
+                <Todoitem
+                  key={todo.id}
+                  todo={todo}
+                  checkCompleted={this.props.checkCompleted}
+                  deleteTodo={this.props.deleteTodo}
+                  saveChanges={this.props.saveChanges}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Todolist
+export default Todolist;
